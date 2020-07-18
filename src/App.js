9e7c1d356ab13434caba832714dbec51e78867f7
liubojun <liubojun904@163.com>
2020-07-18 17:04:34 +0800

    antd的相关配置

--- src/App.js
+++ src/App.js
@@ -1,26 +1,14 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+// 应用的根组件
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react'
+import { Button } from 'antd';
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Button type="primary">Dashed Button</Button>
+      </div>
+    )
+  }
 }
-
-export default App;
