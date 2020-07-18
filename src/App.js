d8a110888429e24041d80b578859a189bdf4f4e6
liubojun <liubojun904@163.com>
2020-07-18 19:44:50 +0800

    路由

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
 // 应用的根组件
-
 import React, { Component } from 'react'
-import { Button } from 'antd';
+import {BrowserRouter,Route,Switch} from 'react-router-dom'
 
+import Login from '.pages/login/login'
+import Admin from '.pages/admin/admin'
+
+// 应用的根组件
 export default class App extends Component {
   render() {
     return (
-      <div>
-        <Button type="primary">Dashed Button</Button>
-      </div>
+      <BrowserRouter>
+        <Switch>
+          <Route path='/login' component={Login}></Route>
+          <Route path='/' component={Admin}></Route>
+        </Switch>
+      </BrowserRouter>
     )
   }
 }
